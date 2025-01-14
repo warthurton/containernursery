03c3186456a0d6e1f559681bf907450dc78216e7
Wayne Arthurton <wayne@itiliti.io>
2025-01-14 11:25:13 -0500

    quick change to let me work

--- build-multiarch.sh
+++ build-multiarch.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-docker buildx build --push --platform linux/amd64,linux/arm64 --tag ghcr.io/itsecholot/containernursery:multiarch .
-regctl image copy ghcr.io/itsecholot/containernursery:multiarch ghcr.io/itsecholot/containernursery:dev
+docker buildx build --push --platform linux/amd64,linux/arm64 --tag ghcr.io/warthurton/containernursery:multiarch .
+docker buildx build --push --platform linux/amd64 --tag ghcr.io/warthurton/containernursery:latest .
+# regctl image copy ghcr.io/warthurton/containernursery:multiarch ghcr.io/warthurton/containernursery:dev 
